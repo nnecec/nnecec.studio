311000555d90dfb728a11f327cadb1847dcd60eb
nnecec <nnecec@outlook.com>
2022-04-13 15:34:11 +0800

    fix: _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,7 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { CssBaseline } from '@nextui-org/react'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return {
-      ...initialProps,
-      styles: <>{initialProps.styles}</>
-    }
-  }
-
   render() {
     return (
       <Html lang="en">
