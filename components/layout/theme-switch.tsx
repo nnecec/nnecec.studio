df67d3f599ee35aa87cf56f43a96f5c227a30ba4
nnecec <nnecec@outlook.com>
2022-04-24 21:28:40 +0800

    fix darkmode switch component initial value

--- components/layout/theme-switch.tsx
+++ components/layout/theme-switch.tsx
@@ -1,18 +1,21 @@
+import { useMemo } from 'react'
 import { Moon, Sun } from '@icon-park/react'
-import { Switch, useTheme } from '@nextui-org/react'
+import { Switch } from '@nextui-org/react'
 import { useTheme as useNextTheme } from 'next-themes'
 
 export const ThemeSwitch = () => {
-  const { setTheme } = useNextTheme()
-  const { isDark } = useTheme()
+  const { theme, setTheme } = useNextTheme()
+
+  const isChecked = useMemo(() => theme === 'dark', [theme])
 
   return (
     <Switch
       aria-label="toggle dark mode"
-      checked={isDark}
+      checked={isChecked}
       onChange={e => setTheme(e.target.checked ? 'dark' : 'light')}
       iconOff={<Sun theme="filled" />}
       iconOn={<Moon theme="filled" />}
+      initialChecked={isChecked}
     ></Switch>
   )
 }
