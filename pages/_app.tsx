7fe6d073f2bd33b4c4b07612b4e55407780463c8
nnecec <nnecec@outlook.com>
2022-05-09 15:00:14 +0800

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { NextUIProvider } from '@nextui-org/react'
 import { AppProps } from 'next/app'
+import Script from 'next/script'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
 import { darkTheme, lightTheme } from 'lib/theme'
@@ -10,18 +11,34 @@ import 'styles/index.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextThemesProvider
-      defaultTheme="system"
-      enableSystem
-      attribute="class"
-      value={{
-        dark: darkTheme.className,
-        light: lightTheme.className
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload" id="gtag">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+
+      <NextThemesProvider
+        defaultTheme="system"
+        enableSystem
+        attribute="class"
+        value={{
+          dark: darkTheme.className,
+          light: lightTheme.className
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+    </>
   )
 }
