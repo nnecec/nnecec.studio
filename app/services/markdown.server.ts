b52278d0b4778258401373cde178e2f66adf80b6
nnecec <nnecec@outlook.com>
2022-08-15 10:56:52 +0800

    feat(detail): add achor

--- app/services/markdown.server.ts
+++ app/services/markdown.server.ts
@@ -8,6 +8,12 @@ marked.setOptions({
   }
 })
 
+const renderer = new marked.Renderer()
+renderer.heading = function (text, level) {
+  return `<a id="${text}" href="#${text}"><h${level} class="hover:after:content-['#'] after:text-primary">${text}</h${level}></a>\n`
+}
+marked.setOptions({ renderer })
+
 export async function markdownToHtml(markdown: string) {
   const result = marked(markdown)
   return result
