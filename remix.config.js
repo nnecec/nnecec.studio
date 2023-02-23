1f04159e9d987d01a8cf4b48b335d5b72abfe360
nnecec <nnecec@outlook.com>
2023-02-23 19:07:12 +0800

    fix: vercel build

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,5 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: 'vercel',
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
-  ignoredRouteFiles: ['**/.*']
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.ts',
+  ignoredRouteFiles: ['**/.*'],
 }
