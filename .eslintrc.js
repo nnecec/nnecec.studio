c6e62862f7c082e9988f45dcf4180245e180ad57
nnecec <nnecec@outlook.com>
2023-03-02 16:20:17 +0800

    chore: update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
+    '@nnecec/eslint-config/react-universal',
     '@remix-run/eslint-config',
     '@remix-run/eslint-config/node',
-    "plugin:tailwindcss/recommended"
-  ]
+  ],
 }
