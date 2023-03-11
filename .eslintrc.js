3e5f03876f32f63610ab1809227a66937eb89bdd
nnecec <nnecec@outlook.com>
2023-03-11 23:34:54 +0800

    feat: route transition

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  ignorePatterns: ['posts/*', 'build/*', 'api/*'],
+  ignorePatterns: ['posts', 'build', 'api', 'public'],
   extends: [
-    '@nnecec/eslint-config/react-universal',
-    // '@remix-run/eslint-config',
-    // '@remix-run/eslint-config/node',
+    // '@nnecec/eslint-config/react-universal',
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
   ],
 }
--- app/components/layout/index.tsx
+++ app/components/layout/index.tsx
@@ -1,4 +1,6 @@
 import type { PropsWithChildren } from 'react'
+import type { Variants } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import { SITE_CONFIG } from '~/utils/constants'
 
@@ -12,6 +14,23 @@ type Props = {
   container?: boolean
 }
 
+const variants: Variants = {
+  exit: {
+    y: 8,
+    opacity: 0,
+    transition: {
+      ease: 'easeInOut',
+    },
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      ease: 'easeInOut',
+    },
+  },
+}
+
 export const Layout = ({
   children,
   title = SITE_CONFIG.title,
@@ -20,9 +39,15 @@ export const Layout = ({
   return (
     <>
       <Header />
-      <main className="container mx-auto min-h-screen px-4 pt-[96px] md:px-0">
+      <motion.main
+        variants={variants}
+        initial="exit"
+        animate="enter"
+        exit="exit"
+        className="container mx-auto min-h-screen px-4 pt-[96px] md:px-0"
+      >
         <div className={className}>{children}</div>
-      </main>
+      </motion.main>
       <Footer />
     </>
   )
