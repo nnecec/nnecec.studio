2f9864d62565089fbd45762d8537bf23bf0961d2
nnecec <nnecec@outlook.com>
2023-03-25 16:51:33 +0800

    fix: styles

--- app/components/theme/switch.tsx
+++ app/components/theme/switch.tsx
@@ -1,5 +1,6 @@
 import { IconMoon, IconSun, IconDeviceLaptop } from '@tabler/icons-react'
-import type { Theme} from './provider';
+import clsx from 'clsx'
+import type { Theme } from './provider'
 import { useTheme } from './provider'
 
 const options = [
@@ -24,17 +25,19 @@ export const ThemeSwitch = () => {
     setTheme(value)
   }
 
+  const active = options.find(option => option.value === theme)
+
   return (
     <div className="dropdown dropdown-end dropdown-hover">
       <label tabIndex={0} className="btn m-1">
-        {options.find(option => option.value === theme)?.label}
+        {active?.label}
       </label>
       <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-200 rounded-box w-52">
         {options.map(({ label, value }) => (
           <li key={value} onClick={() => changeTheme(value as Theme)}>
-            <a>
+            <button className={clsx(active?.value === value && 'active')}>
               {label} {value}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
