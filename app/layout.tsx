268dd11e0c50e729665e00cef3b1e347ac9b6a3b
nnecec <nnecec@outlook.com>
2023-07-11 11:23:35 +0800

    feat: add datapulse

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link href="/favicon/site.webmanifest" rel="manifest" />
         <meta title={SITE_CONFIG.title} />
         <meta content={SITE_CONFIG.description} name="description" />
+        {process.env.NODE_ENV === 'production' && (
+          <script
+            data-endpoint="https://datapulse.app/api/v1/event"
+            data-workspace="cljxpa07r0xjb8j37c7woz5fx"
+            defer
+            id="datapulse"
+            src="https://datapulse.app/datapulse.min.js"
+            type="text/javascript"
+          />
+        )}
       </head>
 
       <body>
